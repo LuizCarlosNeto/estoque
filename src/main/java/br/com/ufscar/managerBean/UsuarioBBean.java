7e0aebde204c87c7b0df9b48184f12c031ecd805
aguiartiago <aguiartiago@hotmail.com>
2014-07-31 00:15:59 -0300

    Correção erro ManagerBean

--- src/main/java/br/com/ufscar/managerBean/UsuarioBBean.java
+++ src/main/java/br/com/ufscar/managerBean/UsuarioBBean.java
@@ -3,12 +3,27 @@ package br.com.ufscar.managerBean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.ufscar.controller.DepartmentController;
+import br.com.ufscar.entity.Department;
+
 @ManagedBean
 @SessionScoped
 public class UsuarioBBean {
-	 
+	private DepartmentController departmentController;
 	private String name;
- 
+
+	public UsuarioBBean() {
+		this.departmentController = new DepartmentController();
+	}
+	
+	public String saveDepartment() {
+		Department department = new Department();
+		department.setName(name);
+		departmentController.persist(department);
+		
+		return "/usuario/usuarioEditar.xhtml";
+	}
+	
 	public String getName() {
 		return name;
 	}
