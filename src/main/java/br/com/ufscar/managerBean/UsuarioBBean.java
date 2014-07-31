bfd06bdbbaf8002c0b93ab9537caff2618654cea
aguiartiago <aguiartiago@hotmail.com>
2014-07-30 23:24:19 -0300

    removido cdi

--- src/main/java/br/com/ufscar/managerBean/UsuarioBBean.java
+++ src/main/java/br/com/ufscar/managerBean/UsuarioBBean.java
@@ -1,14 +1,11 @@
 package br.com.ufscar.managerBean;
 
-import java.io.Serializable;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class UsuarioBBean implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class UsuarioBBean {
 	 
 	private String name;
  
