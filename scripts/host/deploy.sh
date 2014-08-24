4a8ec9188c18bd22319a0790bef3b62cd8a75089
Tiago Aguiar <aguiartiago@hotmail.com>
2014-08-24 11:03:28 -0300

    script

--- scripts/host/deploy.sh
+++ scripts/host/deploy.sh
@@ -2,6 +2,7 @@
 
 rm /usr/local/tomcat7/webapps/*.war
 rm /usr/local/tomcat7/webapps/*.war.*
+rm -Rf /usr/local/tomcat7/webapps/estoque*
 
 cp /root/projetos/estoque/target/estoque*.war /usr/local/tomcat7/webapps
 
--- scripts/host/update.sh
+++ scripts/host/update.sh
@@ -10,8 +10,11 @@ mvn clean install
 
 rm /usr/local/tomcat7/webapps/*.war
 rm /usr/local/tomcat7/webapps/*.war.*
+rm -Rf /usr/local/tomcat7/webapps/estoque*
 
 cp /root/projetos/estoque/target/estoque*.war /usr/local/tomcat7/webapps
+
+chmod 755 /usr/local/tomcat7/webapps/estoque*.war
 
 /usr/local/tomcat7/bin/startup.sh
 
