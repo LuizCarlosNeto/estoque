1e76b894436e675827974bf60b3702c7430584b0
Tiago Aguiar <aguiartiago@hotmail.com>
2014-11-08 11:35:43 -0200

    script

--- scripts/host/update.sh
+++ scripts/host/update.sh
@@ -6,15 +6,15 @@ git pull
 
 mvn clean install
 
-./usr/local/tomcat7/bin/shutdown.sh
+cd /usr/local/tomcat7
+./bin/shutdown.sh
 
-rm /usr/local/tomcat7/webapps/*.war
-rm /usr/local/tomcat7/webapps/*.war.*
-rm -Rf /usr/local/tomcat7/webapps/estoque*
+rm -f webapps/*.war
+rm -f webapps/*.war.*
+rm -Rf webapps/estoque*
 
 cp /root/projetos/estoque/target/estoque*.war /usr/local/tomcat7/webapps
 
 chmod 755 /usr/local/tomcat7/webapps/estoque*.war
 
-./usr/local/tomcat7/bin/startup.sh
-
+./bin/startup.sh
